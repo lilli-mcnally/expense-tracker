3de29c3d5151b425d0addf57084c46e7faab7c7d
lilli-mcnally <lilli.lawlor@hotmail.co.uk>
2026-04-22 22:10:36 +0100

    Initial commit with frontend and backend setup

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://lillilawlor_db_user:hAJbE52r09uWTqJB@cluster0.i0qo3jq.mongodb.net/Expense?")
+}
